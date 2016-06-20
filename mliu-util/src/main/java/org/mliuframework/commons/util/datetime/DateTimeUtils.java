b9c950cf208221e3d5820be90b92462bcc94ad09
baidu <baidu@MacBook-Pro.local>
2016-06-20 09:42:16 +0800

    DateUtils.java updated, add test files

--- mliu-util/src/main/java/org/mliuframework/commons/util/datetime/DateTimeUtils.java
+++ mliu-util/src/main/java/org/mliuframework/commons/util/datetime/DateTimeUtils.java
@@ -19,8 +19,29 @@ public class DateTimeUtils {
         return DateTimeFormatUtils.format(System.currentTimeMillis(), pattern);
     }
 
-    public static Date addDays(final Date datetime, int days) {
+    public static Date addYears(final Date datetime, final int years) {
+        return DateUtils.addYears(datetime, years);
+    }
+
+    public static Date addMonths(final Date datetime, final int months) {
+        return DateUtils.addMonths(datetime, months);
+    }
+
+
+    public static Date addDays(final Date datetime, final int days) {
         return DateUtils.addDays(datetime, days);
+    }
+
+    public static Date addHours(final Date datetime, final int hours) {
+        return DateUtils.addHours(datetime, hours);
+    }
+
+    public static Date addMinutes(final Date datetime, final int minutes) {
+        return DateUtils.addMinutes(datetime, minutes);
+    }
+
+    public static Date addSeconds(final Date datetime, final int seconds) {
+        return DateUtils.addSeconds(datetime, seconds);
     }
 
 }
---
+++ mliu-util/src/test/java/org/mliuframework/commons/util/datetime/package-info.java
@@ -0,0 +1,2 @@
+
+package org.mliuframework.commons.util.datetime;
