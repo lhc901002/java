ef9e6d69b0cff21609bbe2efa7362c2ab071d8e3
baidu <baidu@MacBook-Pro.local>
2016-07-02 18:57:26 +0800

    add factory method pattern

---
+++ designpattern-demo/src/main/java/org/mliuframework/demo/designpattern/structure/factory/factorymethod/Main.java
@@ -0,0 +1,14 @@
+package org.mliuframework.demo.designpattern.structure.factory.factorymethod;
+
+/**
+ * Created by Michael on 7/2/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(CarFactory.buildCar(CarType.SMALL));
+        System.out.println(CarFactory.buildCar(CarType.SEDAN));
+        System.out.println(CarFactory.buildCar(CarType.LUXURY));
+    }
+
+}
