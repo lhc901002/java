52c6c40269e9dc557b1685349433e44d0b5615d6
baidu <baidu@MacBook-Pro.local>
2016-07-04 20:52:09 +0800

    add serialize test method for Java Built-in and Kryo

---
+++ io-demo/src/main/java/org/mliuframework/demo/io/serialization/SerializeByKryo.java
@@ -0,0 +1,60 @@
+package org.mliuframework.demo.io.serialization;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoException;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Michael on 7/4/16.
+ */
+public class SerializeByKryo {
+
+    private static Kryo initKryo() {
+        Kryo kryo = new Kryo();
+        kryo.setReferences(false);
+        kryo.setRegistrationRequired(false);
+        kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
+        return kryo;
+    }
+
+    private static void setSerializableObject(Kryo kryo) throws FileNotFoundException {
+        kryo.register(Student.class);
+        Output output = new Output(new FileOutputStream("student.out"));
+        for (int i = 0; i < 1000000; i++) {
+            kryo.writeObject(output, new Student(i, "Michael" + i, 25, "Computer Science" + i));
+        }
+        output.flush();
+        output.close();
+    }
+
+    private static void getSerializableObject(Kryo kryo) throws FileNotFoundException, KryoException {
+        Input input = new Input(new FileInputStream("student.out"));
+        Student student = null;
+        while((student = kryo.readObject(input, Student.class)) != null){
+            //System.out.println(student);
+        }
+        input.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Kryo kryo = initKryo();
+        long start = System.currentTimeMillis();
+        setSerializableObject(kryo);
+        long end = System.currentTimeMillis();
+        System.out.println("Kryo Serializable writeObject time: " +
+                (end - start) + "ms");
+        start = System.currentTimeMillis();
+        getSerializableObject(kryo);
+        end = System.currentTimeMillis();
+        System.out.println("Kryo Serializable readObject time: " +
+                (end - start) + "ms");
+    }
+
+}
---
+++ io-demo/src/main/java/org/mliuframework/demo/io/serialization/Student.java
@@ -0,0 +1,72 @@
+package org.mliuframework.demo.io.serialization;
+
+import java.io.Serializable;
+
+/**
+ * Created by Michael on 7/4/16.
+ */
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = 3756302160365894878L;
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String department;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, Integer age, String department) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+}
