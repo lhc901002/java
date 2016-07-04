6534c369a56d03eb9e48415ea6306bad27c3e0a3
baidu <baidu@MacBook-Pro.local>
2016-07-04 14:28:10 +0800

    add demo of observer pattern

---
+++ designpattern-demo/src/main/java/org/mliuframework/demo/designpattern/behavior/observer/Observer.java
@@ -0,0 +1,10 @@
+package org.mliuframework.demo.designpattern.behavior.observer;
+
+/**
+ * Created by Michael on 7/4/16.
+ */
+public interface Observer {
+
+    void update(float temperature, float humidity, float pressure);
+
+}
