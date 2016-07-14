66dec08c50c2a2bc950b7ffae651e102d41fcefb
lhc901002 <haocheng.liu@hotmail.com>
2016-07-14 23:23:09 +0800

    rename mliu-collection as java-algorithm

---
+++ java-algorithm/src/main/java/org/mliuframework/java/algorithm/sort/MergeSort.java
@@ -0,0 +1,54 @@
+package org.mliuframework.java.algorithm.sort;
+
+/**
+ * Created by Michael on 2016/6/25.
+ */
+public class MergeSort {
+
+    public static void doSort(int [] array, int order) {
+        if (array.length > 1) {
+            int[] tmpArray = new int [array.length];
+            doSort(array, tmpArray, 0, array.length - 1, order);
+        }
+    }
+
+    public static void doSort(int [] array, int [] tmpArray,
+                              int start, int end, int order) {
+        if (array.length > 1 && start < end) {
+            int middle = (start + end) >> 1;
+            doSort(array, tmpArray, start, middle, order);
+            doSort(array, tmpArray, middle + 1, end, order);
+            merge(array, tmpArray, start, middle, end, order);
+        }
+    }
+
+    private static void merge(int [] array, int [] tmpArray, int start,
+                              int middle, int end, int order) {
+        int i = start, j = middle + 1, k = start;
+        while (i <= middle && j <= end) {
+            if (array[i] <= array[j]) {
+                if (order == SortMark.ASCENDING_ORDER) {
+                    tmpArray[k++] = array[i++];
+                } else {
+                    tmpArray[k++] = array[j++];
+                }
+            } else {
+                if (order == SortMark.ASCENDING_ORDER) {
+                    tmpArray[k++] = array[j++];
+                } else {
+                    tmpArray[k++] = array[i++];
+                }
+            }
+        }
+        while (i <= middle) {
+            tmpArray[k++] = array[i++];
+        }
+        while (j <= end) {
+            tmpArray[k++] = array[j++];
+        }
+        for (int index = start; index <= end; index++) {
+            array[index] = tmpArray[index];
+        }
+    }
+
+}
---
+++ java-algorithm/src/main/java/org/mliuframework/java/algorithm/sort/SortMark.java
@@ -0,0 +1,12 @@
+package org.mliuframework.java.algorithm.sort;
+
+/**
+ * Created by Michael on 2016/6/25.
+ */
+public class SortMark {
+
+    public static final int ASCENDING_ORDER = 1;
+
+    public static final int DESCENDING_ORDER = 2;
+
+}
